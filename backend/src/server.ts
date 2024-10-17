f48df4a78ed08e4712ecaa1857754c09fadcbf65
James Kim <jamesjkjk@gmail.com>
2024-10-17 16:27:35 -0700

    add hello world for / endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,9 @@ app.use(cors({
 app.use(express.json())
 // Define a port for the server to listen on
 const PORT = process.env.PORT || 8080;
-
+app.use('/',(req,res)=>{
+  res.send("Hello World")
+})
 app.use('/api', openf1Routes);
 
 // Start the server and listen on the specified port
